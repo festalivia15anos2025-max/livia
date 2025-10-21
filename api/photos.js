db0cc49a67dac5977d1173f56bbfc6d0cb51d111
festalivia15anos2025-max <festalivia15anos2025@gmail.com>
2025-10-21 08:26:08 -0300

    Update photos.js

--- api/photos.js
+++ api/photos.js
@@ -25,28 +25,53 @@ module.exports = async function handler(req, res) {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       },
-      scopes: ['https://www.googleapis.com/auth/drive.file'],
+      scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file'
+      ],
     });
 
     const drive = google.drive({ version: 'v3', auth });
 
-    // Listar arquivos
+    // Listar TODOS os arquivos da pasta (sem filtro de tipo)
     const response = await drive.files.list({
       q: `'${FOLDER_ID}' in parents and trashed=false`,
-      fields: 'files(id, name, webViewLink, thumbnailLink, createdTime)',
+      fields: 'files(id, name, webViewLink, webContentLink, thumbnailLink, createdTime, mimeType)',
       orderBy: 'createdTime desc',
-      pageSize: 100,
+      pageSize: 1000,
+    });
+
+    console.log('Arquivos encontrados:', response.data.files?.length || 0);
+
+    // Processar todos os arquivos
+    const files = (response.data.files || []).map((file) => {
+      // Gerar thumbnail e link de visualização
+      const thumbnailLink = `https://drive.google.com/thumbnail?id=${file.id}&sz=w400`;
+      const webViewLink = `https://drive.google.com/file/d/${file.id}/view`;
+      const directLink = `https://lh3.googleusercontent.com/d/${file.id}`;
+
+      return {
+        id: file.id,
+        name: file.name,
+        thumbnailLink: thumbnailLink,
+        webViewLink: webViewLink,
+        directLink: directLink,
+        createdTime: file.createdTime,
+        mimeType: file.mimeType,
+      };
     });
 
     return res.status(200).json({ 
       success: true, 
-      files: response.data.files || [] 
+      files: files,
+      count: files.length
     });
   } catch (error) {
     console.error('Erro ao listar fotos:', error);
     return res.status(500).json({ 
       success: false, 
-      error: error.message || 'Erro ao carregar fotos'
+      error: error.message || 'Erro ao carregar fotos',
+      details: error.toString()
     });
   }
 };
